434ef355951937d7b69334fdd0e8316705a72b1f
Hari <hari@Haris-MacBook-Air-2.local>
2024-10-15 11:27:05 +0530

    Initial commit for Flask calculator

---
+++ calculator/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def calculator():
+    result = ''
+    if request.method == 'POST':
+        try:
+            expression = request.form.get('expression')
+            result = eval(expression)
+        except Exception as e:
+            result = "Error"
+    return render_template('index.html', result=result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
